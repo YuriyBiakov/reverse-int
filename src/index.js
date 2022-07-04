b8b4180a24a62913df6ebf3eed1c795d52c6f84f
Yuriy Biakov <gbyackov@gmail.com>
2022-07-04 20:35:29 +0300

    feat: add function  code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = -n;
+    
+    let str = String(n);
+    let length = str.length;
+    let tempStr = '';
+
+    for (let i = 0; i < length; i++){
+    tempStr = str[i] + tempStr;
+    console.log(str[i]);
+    }
+    
+    return +tempStr;
 }
